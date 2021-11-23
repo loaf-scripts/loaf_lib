40eecc4bc26126c3683b07abeb12f4e68a9af6b5
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2021-11-23 19:29:59 +0100

    Delete markersaving.lua

--- server/markersaving.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("loaf_lib:saveMarkers")
-AddEventHandler("loaf_lib:saveMarkers", function(markers)
-    SaveResourceFile(GetCurrentResourceName(), "markers.json", json.encode(markers))
-end)
