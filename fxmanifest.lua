e28e25c9b10cbcbe7e46ab0f86add733bacd4516
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2021-11-28 14:24:20 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,10 @@ client_scripts {
     "client/*.lua",
     "client/functions/*.lua"
 }
-server_script "server/*.lua"
+server_scripts {
+    "server/*.lua",
+    "server/functions/*.lua"
+}
 shared_script "shared/*.lua"
 
 files {
